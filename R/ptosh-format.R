8b52fe01a5669365c6af6b586ea4ee5d85fadf0d
kirimichan <mariko.ohtsuka@nnh.go.jp>
2019-01-15 16:29:51 +0900

    modify

--- R/ptosh-format.R
+++ R/ptosh-format.R
@@ -3,12 +3,12 @@
 # Author: mariko ohtsuka
 
 # library, function section ------
-#' Title
-#'
+#' @title
+#' Exit function
+#' @description
+#' Exit from this program
 #' @return
-#' @export
-#'
-#' @examples
+#' No return value
 Exit <- function(){
   .Internal(.invokeRestart(list(NULL, NULL), NULL))
 }
@@ -34,29 +34,32 @@ CheckColname <- function(col_name, df){
   return(res)
 }
 # Constant section ------
+kPtoshRegistrationNumberColumnIndex = 9  # ptosh_csv$registration_number
+kSheetAliasNameColumnIndex = 1  # sheet.csv$sheet.alias_name
+kSheetFieldItemNameColumnIndex = 3  # sheet.csv$FieldItem.name.tr..field......
 # Main section ------
 
 # Initialize ------
 Sys.setenv("TZ" = "Asia/Tokyo")
 parent_path <- "/Users/admin/Desktop/NHOH-R-miniCHP"
 # Setting of input/output path
-input_path <<- paste0(parent_path, "/input")
+input_path <- paste0(parent_path, "/input")
 external_path <<- paste0(parent_path, "/external")
 # If the output folder does not exist, create it
-output_path <<- paste0(parent_path, "/output")
+output_path <- paste0(parent_path, "/output")
+output_dst_path <- paste0(output_path, "/dst")
 if (file.exists(output_path) == F) {
   dir.create(output_path)
 }
+if (file.exists(output_dst_path) == F) {
+  dir.create(output_dst_path)
+}
 # Input option.csv
 option_csv <- read.csv(paste0(external_path, "/option.csv"), as.is=T, fileEncoding="utf-8", stringsAsFactors=F)
-# Generate factor from option.csv
-option_name_list <- option_csv[!duplicated(option_csv[1]), "Option.name"]
-for (i in 1:length(option_name_list)) {
-  temp <- subset(option_csv, option_csv$Option.name == option_name_list[i])
-  x <- factor
-}
-# Input sheet.csv
-sheet_csv <- read.csv(paste0(external_path, "/testsheet.csv"), as.is=T, fileEncoding="utf-8", stringsAsFactors=F)
+# Input sheet.csv, delete rows that 'variable' is NA
+sheet_csv <- read.csv(paste0(external_path, "/testsheet.csv"), as.is=T, na.strings="", fileEncoding="utf-8",
+                      stringsAsFactors=F)
+sheet_csv <- subset(sheet_csv, !is.na(sheet_csv$variable))
 # Check for duplicate 'variable'
 # Check overlap from the beginning and end, OR of both
 df_duplicated <- sheet_csv[duplicated(sheet_csv$variable) | duplicated(sheet_csv$variable, fromLast=T), ]
@@ -64,51 +67,51 @@ if (nrow(df_duplicated) != 0) {
   df_duplicated <- cbind(rownames(df_duplicated), df_duplicated)
   names(df_duplicated)[1] <- "row"
   write.csv(df_duplicated, paste0(output_path, "/variable_duplicated.csv"), row.names=F, fileEncoding="cp932")
-  stop("not numeric !")
+  stop("Duplicate variable name")
   Exit()
 }
 # Input ptosh_csv
 # Set ptosh_csv's name list
-alias_name <- sheet_csv[!duplicated(sheet_csv[1]), 1]
+alias_name <- sheet_csv[!duplicated(sheet_csv[kSheetAliasNameColumnIndex]), kSheetAliasNameColumnIndex]
 file_list <- list.files(input_path)
 for (i in 1:length(alias_name)) {
   file_index <- grep(paste0("_", alias_name[i] , "_"), file_list)
   if (length(file_index) > 0) {
     assign(alias_name[i], read.csv(paste(input_path, file_list[file_index], sep="/"), as.is=T, na.strings="",
                                    fileEncoding="cp932"))
-    #症例登録番号(I列)をキーにしてループを回す（繰り返しシートの可能性があるため）。
-    #変数名が定義されているフィールド番号を含む列名のデータに対し、マッピング上の変数名にて読み込む。
-    #SASでデータの次の列の文字列をFORMATとしてあてる。
-    #RであればValue Labelとして次の列の文字列をfactorとしてあてる(*)
-
     # Select sheet_csv's rows if sheet_csv$Sheet.alias_name and ptosh_csv$alias_name is same value
-    df_itemlist <- subset(sheet_csv, sheet_csv[ ,1] == alias_name[i])
-    # Set dataset from ptosh_csv
-    df_sheet <- get(alias_name[i])
+    df_itemlist <- subset(sheet_csv, sheet_csv[ ,kSheetAliasNameColumnIndex] == alias_name[i])
+    # Set dataset from ptosh_csv, sort by I column (Registration number)
+    temp_df_sheet <- get(alias_name[i])
+    sortlist <- order(temp_df_sheet[kPtoshRegistrationNumberColumnIndex])
+    df_sheet <- temp_df_sheet[sortlist, ]
     # temp_df_name : value that add "temp" to the head of ptosh_csv name
     # temp_df_sheet : Dataset named temp_df_name, set ptosh_csv
     temp_df_name <- paste0("temp_", alias_name[i])
-    assign(temp_df_name, data.frame(id = df_sheet[9]))
+    assign(temp_df_name, data.frame(id = df_sheet[kPtoshRegistrationNumberColumnIndex]))
     temp_df_sheet <- get(temp_df_name)
-    for (j in 1:length(df_itemlist)) {
+    for (j in 1:length(df_itemlist$variable)) {
       # Get column name from the value of sheet_csv$variable
-      column_name <- sheet_csv[j, "variable"]
+      column_name <- df_itemlist[j, "variable"]
       # Skip if there is no column with that name
-      target_column_name <- paste0("field", sheet_csv[j, 3])
+      target_column_name <- paste0("field", sheet_csv[j, kSheetFieldItemNameColumnIndex])
       target_column_index <- CheckColname(target_column_name, df_sheet)
+      temp_colnames <- c(colnames(temp_df_sheet), column_name)
       if (target_column_index > 0) {
         temp_df_sheet <- cbind(temp_df_sheet, df_sheet[target_column_name])
+        colnames(temp_df_sheet) <- temp_colnames
         # Set option value
-        if (nchar(df_itemlist[j, "Option.name"]) > 0) {
+        if (!is.na(df_itemlist[j, "Option.name"])) {
           temp_factor <- subset(option_csv, option_csv$Option.name == df_itemlist[j, "Option.name"])
-          temp_df_sheet[ ,target_column_name] <- factor(temp_df_sheet[ ,target_column_name],
+          temp_df_sheet[ , column_name] <- factor(temp_df_sheet[ , column_name],
                                                         levels = as.matrix(temp_factor["Option..Value.code"]),
                                                         labels = as.matrix(temp_factor["Option..Value.name"]))
         }
       }
       assign(temp_df_name, temp_df_sheet)
     }
+    # Output csv and R_dataframe
+    write.csv(get(temp_df_name), paste0(output_path, "/", alias_name[i], ".csv"), na='""', row.names=F)
+    save(list=temp_df_name, file=(paste0(output_dst_path, "/", alias_name[i], ".Rda")))
   }
 }
-#sheet.alias_nameのループが終わればデータセットを出力して終了。
-#write.csv(temp_ae, paste(output_path, file_list[i], sep="/"), na='""', row.names=F)
